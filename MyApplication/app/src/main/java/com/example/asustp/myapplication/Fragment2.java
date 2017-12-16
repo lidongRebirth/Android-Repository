622c507d51aacbf31265dc3d23a8ae9c81229ca3
myfittinglife <34064891+myfittinglife@users.noreply.github.com>
2017-12-16 22:46:28 +0800

    setOnTouchListener
    fragment2中的密码框的显示的触发事件

--- MyApplication/app/src/main/java/com/example/asustp/myapplication/Fragment2.java
+++ MyApplication/app/src/main/java/com/example/asustp/myapplication/Fragment2.java
@@ -4,11 +4,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +25,7 @@ public class Fragment2 extends Fragment {
     private TextView xuehao;
     private EditText password;
     private Button passbtn,idbtn;
+    private ImageButton showPassword;
 
 
     @Override
@@ -33,17 +38,66 @@ public class Fragment2 extends Fragment {
         password=view.findViewById(R.id.password);
         passbtn=view.findViewById(R.id.changepass);
         idbtn=view.findViewById(R.id.changeid);
+        showPassword=view.findViewById(R.id.showpassword);
+
 
         xuehao.setText(sp.getString("USER_NAME",""));//从SharePerference中获取学号和密码
         password.setText(sp.getString("PASSWORD",""));
 
-        password.setOnLongClickListener(new View.OnLongClickListener() {
+//        showPassword.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(view.getContext(),"测试",Toast.LENGTH_SHORT).show();;
+//                      password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());//EditText动态显示
+//            }
+//        });
+        showPassword.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onLongClick(View view) {//长按监听事件来显示密码
-                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                return false;
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                   case MotionEvent.ACTION_UP://按住事件后执行代码的区域
+                   {
+                       password.setTransformationMethod(PasswordTransformationMethod.getInstance());//EditText动态呈现密文
+
+                       break;
+                   }
+                   case MotionEvent.ACTION_DOWN://松开事件后执行的代码
+                   {
+                       password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());//EditText动态显示
+
+                       break;
+                   }
+                    default:
+                        break;
+                }
+            return true;
             }
         });
+
+//       idbtn.setOnKeyListener(new View.OnKeyListener() {
+//           @Override
+//           public boolean onKey(View view, int i, KeyEvent keyEvent) {
+//               switch (keyEvent.getAction()){
+//                   case KeyEvent.ACTION_UP://按住事件后执行代码的区域
+//                   {
+//                       password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());//EditText动态显示
+//                   }
+//                   case KeyEvent.ACTION_DOWN://松开事件后执行的代码
+//                   {
+//                        password.setTransformationMethod(PasswordTransformationMethod.getInstance());//EditText动态呈现密文
+//                   }
+//               }
+//               return false;
+//           }
+//       });
+
+//        password.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {//长按监听事件来显示密码
+//                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+//                return false;
+//            }
+//        });
 
 
         passbtn.setOnClickListener(new View.OnClickListener() {//修改密码按钮的点击事件
@@ -54,11 +108,8 @@ public class Fragment2 extends Fragment {
                 Toast.makeText(getContext(),sp.getString("USER_NAME",""),Toast.LENGTH_SHORT).show();//测试时少写show()所以显示不出来
                 Toast.makeText(getContext(),sp.getString("PASSWORD",""),Toast.LENGTH_SHORT).show();//测试时少写show()所以显示不出来
 
-
             }
         });
-
-
         return view;
     }
     @Override
